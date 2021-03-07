3c7892e01d729e80704ee7a22a11b353f177ce3c
Jordan Shipley <jordan.j.shipley@gmail.com>
2021-03-06 22:26:02 -0700

    updating html div index.js

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -28,15 +28,42 @@ const IndexPage = () => {
 
 
     return (
-        <div>
-            <h1>{responseData.title}</h1>
-
-            <button onClick={(e) => fetchData(e)} type='button'>Click Me For Data</button>
-
-            {responseData.dates && responseData.dates.map(date => {
-                return <p>{date}</p>
-            })}
-        </div>
+        <div
+        style={{
+            background: '#EEE',
+            padding: '5%',
+            fontFamily: '"Lucida Console", Monaco, monospace'
+        }}>
+        <h1
+            style={{
+                background: 'black',
+                color: 'white',
+                padding: '1rem',
+                display: 'inline-block'
+            }}>Gatsby Stock Market App</h1>
+        <h2>Analyze Stock Data</h2>
+        <form onSubmit={fetchData}>
+            <fieldset>
+                <legend>Search Stock Market</legend>
+                <label htmlFor="ticker">Enter stock ticker
+                    <input
+                        required
+                        name="ticker"
+                        id="ticker"
+                        type='text'
+                        placeholder='SPY'
+                        value={ticker}
+                        onChange={(e) => setTicker(e.target.value)}
+                    />
+                </label>
+                <button type='submit'>Submit</button>
+            </fieldset>
+        </form>
+        <p>{message}</p>
+        <h3>Symbol: {responseData ? responseData.symbol : ''}</h3>
+        <p>Daily Time Series with Splits and Dividend Events</p>
+        <small>Last Refresh: {responseData ? responseData.refreshed : ''}</small>
+    </div>
     )
 }
 
