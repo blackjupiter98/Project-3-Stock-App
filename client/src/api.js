6b0ce09648ec4ee49cbcf40ce891243b1a69093e
Jordan Shipley <jordan.j.shipley@gmail.com>
2021-03-07 21:34:19 -0700

    updating api.js

--- client/src/api.js
+++ client/src/api.js
@@ -23,5 +23,33 @@ export default {
             'symbol': symbol.toUpperCase()
         },
 
+        transformResponse: [function (data) {
+            // Do whatever you want to transform the data
+            console.log('Transforming data...')
+        
+            const json = JSON.parse(data)
+        
+            const dates = Object.keys(json['Time Series (Daily)']).reverse()
+        
+            // Construct response data for chart input
+            const closePrices = dates.map(date => date = {
+                date,
+                close: Number(json['Time Series (Daily)'][date]['4. close'])
+            })
+        
+            const symbol = json['Meta Data']['2. Symbol']
+            const refreshed = json['Meta Data']['3. Last Refreshed']
+        
+            data = {
+                symbol,
+                refreshed,
+                closePrices
+            }
+        
+            return data;
+        }],
     })
 }
+
+ 
+       
